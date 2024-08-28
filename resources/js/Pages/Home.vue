6aea0032b723ef48bea0c974e310d6b609309412
Nadim Hairi <nadim@runcloud.io>
2024-08-28 10:42:45 +0800

    style: clean up code line

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -82,9 +82,9 @@ const data = reactive({
           </form>
         </CardContent>
         <CardFooter class="flex justify-between px-6 pb-6">
-          <Link :href="route('logout')" method="post" as="button"
-            ><Button variant="outline"> Cancel </Button></Link
-          >
+          <Link :href="route('logout')" method="post" as="button">
+            <Button variant="outline"> Cancel </Button>
+          </Link>
           <Link :href="route('signup')"><Button>Deploy</Button></Link>
         </CardFooter>
       </Card>
