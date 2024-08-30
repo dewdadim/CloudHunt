2029c5f4e99e2965e206a7db48281a54eeaab95e
Nadim Hairi <nadim@runcloud.io>
2024-08-30 16:26:11 +0800

    fe: setup onboard session for new user

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,25 +1,6 @@
 <script setup lang="ts">
 import MaxWidthWrapper from '@/components/MaxWidthWrapper.vue'
-import { Button } from '@/components/ui/button'
 import { reactive } from 'vue'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Link } from '@inertiajs/vue3'
 import Navbar from '@/components/Navbar.vue'
 
 const add = (x: number, y: number) => {
@@ -35,8 +16,8 @@ const data = reactive({
   <Navbar />
   <div class="flex h-screen">
     <aside class="h-full w-80 flex-none border-r"></aside>
-    <MaxWidthWrapper class="h-full flex-initial"
-      ><div class="h-full w-full bg-purple-50"></div
-    ></MaxWidthWrapper>
+    <MaxWidthWrapper class="h-full flex-initial">
+      <div class="h-full w-full bg-purple-50"></div>
+    </MaxWidthWrapper>
   </div>
 </template>
--- resources/js/Pages/Onboard.vue
+++ resources/js/Pages/Onboard.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+import MaxWidthWrapper from '@/components/MaxWidthWrapper.vue'
+import FullNameQuestion from '@/components/onboard/FullNameQuestion.vue'
+import { Progress } from '@/components/ui/progress'
+import { ChevronLeft } from 'lucide-vue-next'
+
+import { ref, watchEffect } from 'vue'
+
+const progress = ref(0)
+
+watchEffect((cleanupFn) => {
+  const timer = setTimeout(() => (progress.value = 25), 500)
+  cleanupFn(() => clearTimeout(timer))
+})
+</script>
+
 <template>
-  <h1>test</h1>
+  <MaxWidthWrapper class="h-screen pt-10">
+    <div class="mb-16 flex items-center gap-4">
+      <ChevronLeft class="flex-none" :size="28" v-on:click="" />
+      <Progress v-model="progress" class="w-6 grow" />
+    </div>
+    <main class="flex flex-col items-center">
+      <FullNameQuestion />
+    </main>
+  </MaxWidthWrapper>
 </template>
