9dda25aa610d16652ccb0cea8e3c060dca2dcd4f
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-09-05 17:35:08 +0800

    chore: tidy up navbar

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -2,56 +2,71 @@
 import { Bell, Inbox, Search } from 'lucide-vue-next'
 import { Button } from './ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import MaxWidthWrapper from './MaxWidthWrapper.vue'
+import { cn } from '@/lib/utils'
 </script>
 
 <template>
-  <nav
-    class="fixed top-0 z-10 flex w-full justify-between border-b bg-background px-6 py-2"
-  >
-    <div class="flex items-center gap-8">
-      <Link :href="route('home')">
-        <img :src="'images/RCEdu.svg'" alt="logo" class="w-28" />
-      </Link>
-      <ul class="flex items-center gap-6">
-        <li><Link href="#">Home</Link></li>
-        <li><Link href="#">Courses</Link></li>
-      </ul>
-    </div>
-    <div class="flex items-center gap-2">
-      <Button class="min-w-64 justify-start gap-2" variant="secondary">
-        <Search />
-        Search
-      </Button>
-      <Button size="icon" variant="secondary">
-        <Inbox :size="20" />
-      </Button>
-      <Button size="icon" variant="secondary">
-        <Bell :size="20" />
-      </Button>
-      <Popover>
-        <PopoverTrigger as-child>
-          <img
-            :src="'images/pfp.jpg'"
-            alt="avatar"
-            class="size-10 cursor-pointer rounded-xl border"
-          />
-        </PopoverTrigger>
-        <PopoverContent class="w-auto min-w-56 max-w-64" align="end">
-          <div class="flex flex-col gap-4">
-            <div class="flex items-center gap-4 rounded-md p-2">
-              <Link
-                :href="route('logout')"
-                method="post"
-                as="button"
-                replace
-                class="w-full py-2 hover:bg-slate-200"
-              >
-                Logout
-              </Link>
+  <nav class="fixed top-0 z-10 w-full border-b bg-background">
+    <MaxWidthWrapper class="flex justify-between py-3">
+      <div class="flex items-center gap-8">
+        <Link :href="route('home')" class="hidden md:inline-flex">
+          <img :src="'images/RCEdu.svg'" alt="logo" class="w-28" />
+        </Link>
+        <ul class="flex items-center gap-6">
+          <li>
+            <Link
+              :href="route('home')"
+              :class="$page.url == '/' ? 'border-b-2 font-bold' : ''"
+              >Home</Link
+            >
+          </li>
+          <li>
+            <Link
+              :href="route('courses')"
+              :class="$page.url == '/courses' ? 'border-b-2 font-bold' : ''"
+              >Courses</Link
+            >
+          </li>
+          <li><Link href="#">News</Link></li>
+        </ul>
+      </div>
+      <div class="flex items-center gap-2">
+        <Button class="w-48 justify-start gap-2 bg-white/50" variant="ghost">
+          <Search :size="20" />
+          <p class="hidden md:inline">Search</p>
+        </Button>
+        <!-- <Button size="icon" variant="ghost">
+          <Inbox :size="20" />
+        </Button> -->
+        <Button size="icon" variant="ghost" class="bg-white/50">
+          <Bell :size="20" />
+        </Button>
+        <Popover>
+          <PopoverTrigger as-child>
+            <img
+              :src="'images/pfp.jpg'"
+              alt="avatar"
+              class="size-10 cursor-pointer rounded-xl"
+            />
+          </PopoverTrigger>
+          <PopoverContent class="w-auto min-w-56 max-w-64" align="end">
+            <div class="flex flex-col gap-4">
+              <div class="flex items-center gap-4 rounded-md p-2">
+                <Link
+                  :href="route('logout')"
+                  method="post"
+                  as="button"
+                  replace
+                  class="w-full py-2 hover:bg-slate-200"
+                >
+                  Logout
+                </Link>
+              </div>
             </div>
-          </div>
-        </PopoverContent>
-      </Popover>
-    </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+    </MaxWidthWrapper>
   </nav>
 </template>
