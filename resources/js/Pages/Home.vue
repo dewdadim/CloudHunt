aee386b4e27fd222c8679e545661aa31525bdb40
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-09-05 17:35:54 +0800

    chore: make home layout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,6 +2,7 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper.vue'
 import { reactive } from 'vue'
 import Navbar from '@/components/Navbar.vue'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 
 const add = (x: number, y: number) => {
   return x + y
@@ -15,9 +16,36 @@ const data = reactive({
 <template>
   <Navbar />
   <div class="flex h-screen">
-    <aside class="h-full w-80 flex-none border-r"></aside>
-    <MaxWidthWrapper class="h-full flex-initial">
-      <div class="h-full w-full bg-purple-50"></div>
+    <MaxWidthWrapper
+      class="mt-28 max-w-[450px] md:max-w-screen-sm md:px-36 lg:max-w-screen-xl"
+    >
+      <!-- <MaxWidthWrapper> -->
+      <div class="flex flex-col justify-center gap-8 lg:flex-row">
+        <div class="relative w-full lg:w-[400px]">
+          <div class="static w-full lg:fixed lg:w-[400px]">
+            <h2 class="text-xl font-bold">Welcome, Nadim</h2>
+            <Card class="mt-6 w-full shadow-none">
+              <CardHeader>
+                <CardTitle>Jump back!</CardTitle>
+              </CardHeader>
+            </Card>
+          </div>
+        </div>
+        <div class="flex grow flex-col gap-2">
+          <h2 class="text-xl font-bold">Welcome, Nadim</h2>
+          <Card class="mt-4 h-[400px]">
+            <CardHeader>
+              <CardTitle>Section 1</CardTitle>
+            </CardHeader>
+          </Card>
+          <Card class="h-[300px]">
+            <CardHeader>
+              <CardTitle>Section 2</CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
+      </div>
+      <!-- </MaxWidthWrapper> -->
     </MaxWidthWrapper>
   </div>
 </template>
