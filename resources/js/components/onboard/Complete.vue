70c70084816af6b2f17aaa22961398c3dbca5240
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-09-06 17:21:00 +0800

    style: make confetti fade away

--- resources/js/components/onboard/Complete.vue
+++ resources/js/components/onboard/Complete.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import ConfettiExplosion from 'vue-confetti-explosion'
-import { Button } from '../ui/button'
 </script>
 
 <template>
-  <div class="flex flex-col items-center gap-2 text-center">
-    <ConfettiExplosion :particleCount="200" :force="0.3" />
+  <div class="flex flex-col items-center gap-2 overflow-visible text-center">
+    <ConfettiExplosion
+      class="animate-disappear"
+      :stageWidth="3000"
+      :duration="3000"
+    />
     <p class="mb-6 pl-8 text-9xl">🎉</p>
     <p>
       The bright future has been set for you! Start learning with RunCloud Edu.
--- tailwind.config.js
+++ tailwind.config.js
@@ -83,12 +83,17 @@ module.exports = {
           from: { height: 'var(--radix-collapsible-content-height)' },
           to: { height: 0 },
         },
+        disappear: {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'collapsible-down': 'collapsible-down 0.2s ease-in-out',
         'collapsible-up': 'collapsible-up 0.2s ease-in-out',
+        disappear: 'disappear 3s ease-in-out',
       },
     },
   },
