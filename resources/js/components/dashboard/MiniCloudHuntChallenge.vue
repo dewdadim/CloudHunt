2a11d1808fd3a45721752a055e1a95463876cbce
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-09-12 17:13:34 +0800

    feat: add champions' avatar

--- resources/js/components/dashboard/MiniCloudHuntChallenge.vue
+++ resources/js/components/dashboard/MiniCloudHuntChallenge.vue
@@ -9,20 +9,28 @@ import {
   CardContent,
   CardFooter,
 } from '../ui/card'
+import { Link } from '@inertiajs/vue3'
+import { cn } from '@/lib/utils'
 
 const ranks = [
   {
+    id: 1,
     badge: '../badges/cloudhunt_rank-2.svg',
-    champion_user: 'nureeninsyirah',
+    champion_avatar: '../images/pfp.jpg',
+    champion_user: 'nureeninsyirah888888',
     time: '00:05:21',
   },
   {
+    id: 2,
     badge: '../badges/cloudhunt_rank-1.svg',
+    champion_avatar: '../images/pfp.jpg',
     champion_user: 'nadimhairi',
     time: '00:04:23',
   },
   {
+    id: 3,
     badge: '../badges/cloudhunt_rank-3.svg',
+    champion_avatar: '../images/pfp.jpg',
     champion_user: 'anggun',
     time: '00:05:45',
   },
@@ -37,26 +45,49 @@ const ranks = [
         Unleash your skills with weekly Mini CloudHunt Challenge!
       </CardDescription>
     </CardHeader>
-    <CardContent class="mt-4 flex flex-col items-center gap-4">
+    <CardContent class="flex flex-col items-center">
       <h4 class="text-lg font-semibold">Previous Champion</h4>
-      <div class="flex max-w-96 gap-2 md:gap-4">
-        <div v-for="rank in ranks">
-          <div class="flex min-w-20 flex-col items-center gap-2">
-            <img :src="rank.badge" class="z-10 size-full" />
+      <div class="flex h-full max-w-96 items-end gap-2 lg:gap-6">
+        <div
+          v-for="rank in ranks"
+          class="rounded-t- h-fit bg-gradient-to-b from-slate-200/0 from-5% via-slate-200/20 to-slate-500/10 pt-2"
+        >
+          <div
+            :class="
+              cn(
+                'flex min-w-20 flex-col items-center gap-1',
+                rank.id === 1
+                  ? 'mb-6'
+                  : rank.id === 2
+                    ? 'mb-12'
+                    : rank.id === 3
+                      ? 'mb-3'
+                      : '',
+              )
+            "
+          >
+            <img :src="rank.badge" class="size-32" />
             <div
-              class="flex flex-col items-center gap-1 text-xs md:text-sm lg:text-base"
+              class="flex flex-col items-center text-xs md:text-sm lg:text-base"
             >
-              <div class="max-w-20 overflow-hidden md:max-w-full">
-                <p class="animate-to-left lg:animate-none">
-                  @{{ rank.champion_user }}
-                </p>
+              <img
+                :src="rank.champion_avatar"
+                class="w-1/3 rounded-lg border"
+              />
+              <div class="w-24 overflow-hidden text-center">
+                <Link
+                  :href="route('profile', { id: rank.champion_user })"
+                  class="group"
+                >
+                  <p class="truncate text-sm group-hover:underline">
+                    @{{ rank.champion_user }}
+                  </p>
+                </Link>
               </div>
-              <p>{{ rank.time }}</p>
+              <p class="flex items-center gap-0.5 text-xs">
+                <Timer :size="14" />{{ rank.time }}
+              </p>
             </div>
-
-            <!-- <div
-              class="absolute top-8 h-52 w-[100%-20] bg-gradient-to-b from-white/20 to-white/0 md:w-28"
-            /> -->
           </div>
         </div>
       </div>
