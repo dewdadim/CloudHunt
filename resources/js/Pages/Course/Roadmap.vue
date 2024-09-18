f9d48da1b2c6a60b02e5c894f58da333c1c59fd6
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-09-18 16:53:10 +0800

    fe(ui): create roadmap page

---
+++ resources/js/Pages/Course/Roadmap.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import Navbar from './Navbar.vue'
+import MaxWidthWrapper from '@/components/MaxWidthWrapper.vue'
+
+const { course } = defineProps<{
+  course: {
+    title: string
+    id: number
+  }
+  chapters: {
+    title: string
+    id: number
+  }
+}>()
+</script>
+
+<template>
+  <Navbar :course="course" />
+  <MaxWidthWrapper class="md:max-w-screen-xl">
+    <div class="flex w-full items-center justify-center">
+      <Card class="shadow-0 mt-20 w-full gap-4 md:w-5/6">
+        <CardHeader class="text-center">
+          <CardDescription>Chapter 1</CardDescription>
+          <CardTitle>Introduction to Cloud Computing</CardTitle>
+        </CardHeader>
+        <CardContent class="h-[500px]"> </CardContent>
+      </Card>
+    </div>
+  </MaxWidthWrapper>
+</template>
