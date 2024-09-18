310b0966b225c6cd4d52bf5f603f31fbb38754f0
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-09-18 16:51:18 +0800

    fe: create navbar for course page

---
+++ resources/js/Pages/Course/Navbar.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { HelpCircle, Slash, StarsIcon } from 'lucide-vue-next'
+
+import MaxWidthWrapper from '@/components/MaxWidthWrapper.vue'
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+} from '@/components/ui/breadcrumb'
+import { Button } from '@/components/ui/button'
+
+const { course } = defineProps<{
+  course: {
+    title: string
+    id: number
+  }
+}>()
+</script>
+
+<template>
+  <nav class="sticky top-0 z-10 w-full">
+    <MaxWidthWrapper
+      class="flex items-center justify-between py-4 md:max-w-screen-2xl"
+    >
+      <div class="flex flex-wrap items-center gap-4">
+        <img
+          :src="'../images/RCEdu.svg'"
+          alt="logo"
+          class="hidden w-28 md:inline-flex"
+        />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <!-- <BreadcrumbItem class="pr-4">
+              <BreadcrumbLink>
+                <img :src="'../images/RCEdu.svg'" alt="logo" class="w-28" />
+              </BreadcrumbLink>
+            </BreadcrumbItem> -->
+            <BreadcrumbItem>
+              <BreadcrumbLink :href="route('dashboard')">
+                Dashboard
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator class="hidden md:inline-flex">
+              <Slash />
+            </BreadcrumbSeparator>
+            <BreadcrumbItem class="hidden md:inline-flex">
+              <BreadcrumbLink :href="route('courses')">
+                Courses
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator>
+              <Slash />
+            </BreadcrumbSeparator>
+            <BreadcrumbItem>
+              <BreadcrumbPage class="max-w-48 truncate md:max-w-full">{{
+                course.title
+              }}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <div class="flex items-center gap-2">
+        <Button
+          variant="ghost"
+          class="flex items-center gap-2 text-lg font-semibold"
+        >
+          <StarsIcon fill="#F9BF3B" class="text-primary" />
+          200
+        </Button>
+        <Button variant="ghost" size="icon">
+          <HelpCircle />
+        </Button>
+      </div>
+    </MaxWidthWrapper>
+  </nav>
+</template>
