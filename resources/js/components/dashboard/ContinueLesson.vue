951f344388df9338d4129e609b31d545eae1d828
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-09-26 16:23:28 +0800

    refactor: make a seperate component for continue lesson

---
+++ resources/js/components/dashboard/ContinueLesson.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3'
+import CourseCard from '../CourseCard.vue'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import { ScrollArea, ScrollBar } from '../ui/scroll-area'
+
+defineProps<{
+  class?: string
+  courses: Course[]
+}>()
+</script>
+
+<template>
+  <Card :class="class">
+    <CardHeader>
+      <CardTitle>Continue Lesson</CardTitle>
+    </CardHeader>
+    <CardContent>
+      <div class="flex">
+        <ScrollArea className="w-1 flex-1" :scroll-hide-delay="100">
+          <div class="flex w-max space-x-4">
+            <div v-for="course in courses">
+              <CourseCard
+                :key="course.id"
+                :title="course.title!"
+                :uri="course.uri!"
+              />
+            </div>
+          </div>
+          <ScrollBar
+            orientation="horizontal"
+            class="transition-opacity ease-in-out"
+          />
+        </ScrollArea>
+      </div>
+    </CardContent>
+  </Card>
+</template>
