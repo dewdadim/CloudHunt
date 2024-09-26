fdc22d28d88e9d322a96ae7740dad62a1b43ab9c
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-09-26 16:25:47 +0800

    chore: remove unnecessary comment

--- resources/js/components/CourseCard.vue
+++ resources/js/components/CourseCard.vue
@@ -13,25 +13,6 @@ const props = defineProps<{
 </script>
 
 <template>
-  <!-- <Card class="shadow-0 p-0">
-    <CardHeader class="p-2">
-      <div className="w-64">
-        <AspectRatio :ratio="3 / 2" class="rounded-xl bg-muted">
-          <img
-            src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-            alt="Photo by Drew Beamer"
-            class="h-full w-full rounded-xl object-cover"
-          />
-        </AspectRatio>
-      </div>
-    </CardHeader>
-    <CardContent class="p-2">
-      <h3 class="text-xl font-semibold">Cybersecurity 2</h3>
-    </CardContent>
-    <CardFooter class="p-2">
-      <Button variant="secondary" class="w-full">Continue Lesson</Button>
-    </CardFooter>
-  </Card> -->
   <div :className="cn('w-72', props.class)">
     <Link :href="route('courses.show', { id: props.uri })">
       <AspectRatio :ratio="7 / 3" class="rounded-xl bg-muted">
