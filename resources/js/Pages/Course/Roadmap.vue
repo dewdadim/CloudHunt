ae1f21097c28e768c4348fbb0032a8da1a9d7657
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-09-27 17:44:44 +0800

    chore: change component props

--- resources/js/Pages/Course/Roadmap.vue
+++ resources/js/Pages/Course/Roadmap.vue
@@ -8,35 +8,20 @@ import {
 } from '@/components/ui/card'
 import Navbar from './Navbar.vue'
 import MaxWidthWrapper from '@/components/MaxWidthWrapper.vue'
-import {
-  CheckSquare,
-  ChevronLeft,
-  ChevronRight,
-  Dumbbell,
-  MonitorPlay,
-  MousePointerClick,
-  Puzzle,
-} from 'lucide-vue-next'
+import { MonitorPlay, MousePointerClick, Puzzle } from 'lucide-vue-next'
 import { cn } from '@/lib/utils'
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 import { Link } from '@inertiajs/vue3'
+import { computed, ref } from 'vue'
+import { Button } from '@/components/ui/button'
 
-const { course, chapter, modules } = defineProps<{
+const { course } = defineProps<{
   course: Course
-  chapter: {
-    title: string
-    id: number
-  }
-  modules: {
-    title: string
-    id: number
-    type: string
-    isDone: false
-  }[]
 }>()
 
-const url = new URL(window.location.href)
-const chapterNumber = parseInt(url.searchParams.get('chapter')!)
+const chapters = computed<Chapter[]>(() => course.chapters!)
+
+const currentChapter = ref(0)
 </script>
 
 <template>
@@ -47,22 +32,27 @@ const chapterNumber = parseInt(url.searchParams.get('chapter')!)
         <CardHeader
           class="flex-row items-center justify-center gap-4 text-center md:gap-8"
         >
-          <Link
-            :href="route('courses.show', { id: course.uri! })"
-            :data="{ chapter: chapterNumber! - 1 }"
+          <Button
+            v-on:click="currentChapter -= 1"
+            :disabled="currentChapter === 0"
+            >Previous</Button
           >
-            <ChevronLeft />
-          </Link>
           <div>
-            <CardDescription>Chapter {{ chapterNumber }}</CardDescription>
-            <CardTitle>{{ chapter.title }}</CardTitle>
+            <CardDescription>
+              Chapter
+              {{
+                chapters
+                  .map((e) => e.uri)
+                  .indexOf(chapters[currentChapter].uri) + 1
+              }}
+            </CardDescription>
+            <CardTitle>{{ chapters[currentChapter].title }}</CardTitle>
           </div>
-          <Link
-            :href="route('courses.show', { id: course.uri! })"
-            :data="{ chapter: chapterNumber! + 1 }"
+          <Button
+            v-on:click="currentChapter += 1"
+            :disabled="currentChapter >= chapters.length - 1"
+            >Next</Button
           >
-            <ChevronRight />
-          </Link>
         </CardHeader>
         <CardContent
           class="mt-2 flex flex-col items-center p-0 md:mt-12 lg:flex-row lg:justify-center"
@@ -71,7 +61,7 @@ const chapterNumber = parseInt(url.searchParams.get('chapter')!)
             <div class="flex flex-col pt-6 md:p-6 md:pb-12 lg:flex-row">
               <div
                 class="grid w-max grid-cols-2 lg:grid-cols-1"
-                v-for="(i, index) in modules"
+                v-for="(i, index) in chapters[currentChapter].modules"
                 :key="i.id"
               >
                 <Link
@@ -79,7 +69,7 @@ const chapterNumber = parseInt(url.searchParams.get('chapter')!)
                   :class="
                     cn(
                       'group relative flex h-56 items-center transition hover:-translate-y-2 hover:cursor-pointer',
-                      modules.indexOf(i) % 2 !== 0
+                      chapters[currentChapter].modules.indexOf(i) % 2 !== 0
                         ? 'order-last'
                         : 'order-first justify-end',
                     )
@@ -97,15 +87,14 @@ const chapterNumber = parseInt(url.searchParams.get('chapter')!)
                   >
                     <component
                       :is="
-                        i.type == 'video'
+                        i.category == 'video'
                           ? MonitorPlay
-                          : i.type == 'activity'
-                            ? MousePointerClick
-                            : Puzzle
+                          : i.category == 'quiz'
+                            ? Puzzle
+                            : MousePointerClick
                       "
                       class="size-14 stroke-[1.2px]"
                     />
-                    <!-- <MonitorPlay :size="52" :stroke-width="1" /> -->
                     <h3 class="text-wrap font-semibold">{{ i.title }}</h3>
                   </div>
                 </Link>
@@ -113,18 +102,18 @@ const chapterNumber = parseInt(url.searchParams.get('chapter')!)
                   :class="
                     cn(
                       'flex h-64 w-full items-end',
-                      modules.indexOf(i) % 2 !== 0
+                      chapters[currentChapter].modules.indexOf(i) % 2 !== 0
                         ? 'justify-end lg:justify-end'
                         : 'justify-start lg:items-start lg:justify-end',
                     )
                   "
                 >
                   <div
-                    v-if="index + 1 !== modules.length"
+                    v-if="index + 1 !== chapters[currentChapter].modules.length"
                     :class="
                       cn(
                         '-z-10 h-2/3 w-1/2 rounded-bl-[52px] border-b-4 border-l-4 border-dashed',
-                        modules.indexOf(i) % 2 !== 0
+                        chapters[currentChapter].modules.indexOf(i) % 2 !== 0
                           ? 'scale-y-[-1]'
                           : 'rotate-180 lg:rotate-0',
                         i.isDone ? 'border-primary' : 'border-slate-400',
