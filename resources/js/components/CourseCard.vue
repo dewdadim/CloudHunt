0c66bb66f0c4ecc75afcc1b985bca3917c268440
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-10-01 17:19:37 +0800

    fix: make border radius larger

--- resources/js/components/CourseCard.vue
+++ resources/js/components/CourseCard.vue
@@ -6,23 +6,24 @@ import { Link } from '@inertiajs/vue3'
 
 const props = defineProps<{
   class?: HTMLAttributes['class']
-  courses?: Course[]
   title: string
   uri: string
 }>()
 </script>
 
 <template>
-  <div :className="cn('w-72', props.class)">
+  <div :className="cn('w-72 rounded-2xl', props.class)">
     <Link :href="route('courses.show', { id: props.uri })">
       <AspectRatio :ratio="7 / 3" class="rounded-xl bg-muted">
         <img
           src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
           alt="Photo by Drew Beamer"
-          class="h-full w-full rounded-t-xl border-x border-t object-cover"
+          class="h-full w-full rounded-t-2xl border-x border-t object-cover"
         />
       </AspectRatio>
-      <div class="h-16 w-full rounded-b-xl border-x border-b px-3 py-1.5">
+      <div
+        class="h-16 w-full rounded-b-2xl border-x border-b bg-card px-3 py-1.5"
+      >
         <h3 class="text-md line-clamp-2 font-semibold">{{ props.title }}</h3>
       </div>
     </Link>
