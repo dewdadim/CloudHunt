8b60aa79403139e71090573189bd4ece204a8262
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-10-03 17:27:16 +0800

    fix(ui): increase course card border radius

--- resources/js/components/CourseCard.vue
+++ resources/js/components/CourseCard.vue
@@ -12,19 +12,21 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div :className="cn('w-72 rounded-2xl', props.class)">
+  <div :className="cn('w-72 rounded-3xl shadow-taper', props.class)">
     <Link :href="route('courses.show', { id: props.uri })">
-      <AspectRatio :ratio="7 / 3" class="rounded-xl bg-muted">
+      <AspectRatio :ratio="7 / 3" class="rounded-3xl bg-muted">
         <img
           src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
           alt="Photo by Drew Beamer"
-          class="h-full w-full rounded-t-2xl border-x border-t object-cover"
+          class="h-full w-full rounded-t-3xl border-x border-t object-cover"
         />
       </AspectRatio>
       <div
-        class="h-16 w-full rounded-b-2xl border-x border-b bg-card px-3 py-1.5"
+        class="h-16 w-full rounded-b-3xl border-x border-b bg-card px-3 py-1.5"
       >
-        <h3 class="text-md line-clamp-2 font-semibold">{{ props.title }}</h3>
+        <h3 class="text-md line-clamp-2 text-wrap font-semibold">
+          {{ props.title }}
+        </h3>
       </div>
     </Link>
   </div>
