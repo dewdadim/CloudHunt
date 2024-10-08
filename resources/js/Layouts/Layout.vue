dd63fce567de3e4fcf20dd2836b87bfee5cc047e
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-10-08 17:02:56 +0800

    feat: add navbar profile menu

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import MaxWidthWrapper from '@/components/MaxWidthWrapper.vue'
 import Navbar from '@/components/Navbar.vue'
+import { usePage } from '@inertiajs/vue3'
+import { computed } from 'vue'
+
+const page = usePage()
+interface User {
+  avatar: string
+  name: string
+}
+
+const user = computed(() => page.props.auth.user)
 </script>
 
 <template>
-  <Navbar />
+  <Navbar
+    :avatar="user.avatar!"
+    :prefer_name="user.prefer_name"
+    :username="user.username"
+  />
   <MaxWidthWrapper class="">
     <slot />
   </MaxWidthWrapper>
--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
-import { Bell, Inbox, Search } from 'lucide-vue-next'
+import {
+  Bell,
+  HelpCircle,
+  Inbox,
+  LogOut,
+  Search,
+  Settings,
+  User,
+} from 'lucide-vue-next'
 import { Button } from './ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
 import MaxWidthWrapper from './MaxWidthWrapper.vue'
+import { Separator } from './ui/separator'
+
+const props = defineProps<{
+  avatar: string
+  prefer_name: string
+  username: string
+}>()
 </script>
 
 <template>
@@ -17,51 +32,81 @@ import MaxWidthWrapper from './MaxWidthWrapper.vue'
             <Link
               :href="route('dashboard')"
               :class="$page.url == '/dashboard' ? 'border-b-2 font-bold' : ''"
-              >Dashboard</Link
             >
+              Dashboard
+            </Link>
           </li>
           <li>
             <Link
               :href="route('courses')"
               :class="$page.url == '/courses' ? 'border-b-2 font-bold' : ''"
-              >Courses</Link
             >
+              Courses
+            </Link>
           </li>
-          <li><Link href="#">News</Link></li>
         </ul>
       </div>
       <div class="flex items-center gap-2">
-        <!-- <Button class="w-48 justify-start gap-2 bg-white/50" variant="ghost">
-          <Search :size="20" />
-          <p class="hidden md:inline">Search</p>
-        </Button> -->
-        <!-- <Button size="icon" variant="ghost">
-          <Inbox :size="20" />
-        </Button> -->
         <Button size="icon" variant="ghost" class="bg-white/50">
           <Bell :size="20" />
         </Button>
         <Popover>
-          <PopoverTrigger as-child>
-            <img
-              :src="'../images/pfp.jpg'"
-              alt="avatar"
-              class="size-10 cursor-pointer rounded-xl"
-            />
+          <PopoverTrigger class="cursor-pointer" as-child>
+            <img :src="props.avatar" alt="avatar" class="size-10 rounded-xl" />
           </PopoverTrigger>
-          <PopoverContent class="w-auto min-w-56 max-w-64" align="end">
-            <div class="flex flex-col gap-4">
-              <div class="flex items-center gap-4 rounded-md p-2">
-                <Link
-                  :href="route('logout')"
-                  method="post"
-                  as="button"
-                  replace
-                  class="w-full py-2 hover:bg-slate-200"
-                >
-                  Logout
-                </Link>
+          <PopoverContent class="w-auto min-w-60 max-w-64" align="end">
+            <div class="flex flex-col gap-2">
+              <div class="flex items-center gap-4 py-4">
+                <img
+                  :src="props.avatar"
+                  alt="avatar"
+                  class="size-16 rounded-xl border"
+                />
+                <div>
+                  <h3 class="font-semibold">{{ props.prefer_name }}</h3>
+                  <p class="text-xs">@{{ props.username }}</p>
+                </div>
               </div>
+
+              <Separator />
+
+              <Link
+                :href="route('users.show', { id: props.username })"
+                class="flex items-center justify-start gap-2 rounded-md p-2 hover:bg-accent"
+              >
+                <User :size="20" />
+                Profile
+              </Link>
+
+              <Separator />
+
+              <Link
+                :href="'#'"
+                class="flex cursor-pointer items-center justify-start gap-2 rounded-md p-2 hover:bg-accent"
+              >
+                <HelpCircle :size="20" />
+                Help & Supports
+              </Link>
+              <Link
+                :href="'#'"
+                class="flex cursor-pointer items-center justify-start gap-2 rounded-md p-2 hover:bg-accent"
+              >
+                <Settings :size="20" />
+                Settings
+              </Link>
+
+              <Separator />
+
+              <Link
+                :href="route('logout')"
+                method="post"
+                as="button"
+                replace
+                class="flex cursor-pointer items-center justify-start gap-2 rounded-md p-2 hover:bg-accent"
+              >
+                <LogOut :size="20" />
+                Logout
+              </Link>
             </div>
           </PopoverContent>
         </Popover>
