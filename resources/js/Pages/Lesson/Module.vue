4489fe9649d13815522181dd3e59eeb1ca46a16e
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-10-21 17:05:33 +0800

    feat: add module task dynamic progress bar

--- resources/js/Pages/Lesson/Module.vue
+++ resources/js/Pages/Lesson/Module.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import MaxWidthWrapper from '@/components/MaxWidthWrapper.vue'
-import { defineAsyncComponent, shallowRef } from 'vue'
+import { Progress } from '@/components/ui/progress'
+import { cn } from '@/lib/utils'
+import { X } from 'lucide-vue-next'
+import { computed, defineAsyncComponent, ref, shallowRef } from 'vue'
 
 const { lesson, module } = defineProps<{
   lesson: Lesson
   module: Module
 }>()
 
+//////////////////////////////////////////////////////////////////////////////
 const component = shallowRef<ReturnType<typeof defineAsyncComponent> | null>(
   null,
 )
@@ -18,10 +22,54 @@ try {
 } catch (error) {
   console.error('Error loading component: ', error)
 }
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// Handle the task update event from the child
+const totalTasks = ref(0)
+const currentTask = ref(0)
+
+// Function to set the total number of tasks
+const setTotalTasks = (total: any) => {
+  totalTasks.value = total
+}
+
+// Function to set the current task
+const setCurrentTask = (current: any) => {
+  currentTask.value = current
+}
+
+// Calculate the progress percentage based on the current step
+const progressPercentage = computed(() => {
+  return ((currentTask.value + 1) / totalTasks.value) * 100
+})
+const progress = ref(progressPercentage)
+//////////////////////////////////////////////////////////////////////////////
 </script>
 <template>
-  <MaxWidthWrapper class="pt-10">
-    <component :is="component" v-if="component" v-bind="{ lesson, module }" />
-    <p v-else>Component not found</p>
-  </MaxWidthWrapper>
+  <main>
+    <MaxWidthWrapper class="pt-10">
+      <div class="flex items-center gap-4">
+        <X
+          :class="
+            cn(
+              'flex-none',
+              !currentTask ? 'cursor-not-allowed' : 'cursor-pointer',
+            )
+          "
+          :size="28"
+          :disabled="currentTask"
+        />
+        <Progress v-model="progress" class="w-6 grow" />
+      </div>
+      <component
+        :is="component"
+        v-if="component"
+        v-bind="{ lesson, module }"
+        @total-tasks="setTotalTasks"
+        @current-task="setCurrentTask"
+      />
+      <p v-else>Component not found</p>
+    </MaxWidthWrapper>
+  </main>
 </template>
---
+++ resources/js/components/lessons/fundamentals-of-cloud-computing/what-is-cloud.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { onMounted, ref } from 'vue'
+
+const emit = defineEmits(['total-tasks', 'current-task'])
+
+const tasks = [1, 2, 3, 4, 5, 2, 2]
+
+const currentTask = ref(0)
+
+onMounted(() => {
+  emit('total-tasks', tasks.length)
+})
+const nextTask = async () => {
+  if (currentTask.value < tasks.length - 1) {
+    currentTask.value++
+    console.log(currentTask.value)
+    emit('current-task', currentTask.value)
+  }
+}
+</script>
+
+<template>
+  <section class="my-16 flex flex-col items-center gap-2">
+    <div class="flex flex-col justify-center gap-4 text-center">
+      <h3 class="text-6xl">🛠</h3>
+      <h2 class="text-xl font-bold">"What is Cloud?" Module Generated</h2>
+      <p class="break-all">
+        This vue file is auto generated, you can edit it at <br />
+        /Users/nadimhairi/Documents/RunCloudEducation/app/../resources/js/components/lessons/fundamentals-of-cloud-computing/what-is-cloud.vue
+      </p>
+      <p>
+        <span class="font-bold">Lesson: </span>Fundamentals of Cloud Computing
+      </p>
+      <p><span class="font-bold">Module: </span>What is Cloud?</p>
+    </div>
+    <Button @click="nextTask">Test</Button>
+  </section>
+</template>
