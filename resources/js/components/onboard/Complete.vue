fcc03a4abaf22d6b09ad7f66853d79077ee3a316
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-11-04 16:00:41 +0800

    chore: install tailwindcss-motion dependencies from rombo.co

--- resources/js/components/onboard/Complete.vue
+++ resources/js/components/onboard/Complete.vue
@@ -3,13 +3,18 @@ import ConfettiExplosion from 'vue-confetti-explosion'
 </script>
 
 <template>
-  <div class="flex flex-col items-center gap-2 overflow-visible text-center">
+  <div class="flex flex-col items-center gap-2 text-center">
     <ConfettiExplosion
       class="animate-disappear"
-      :stageWidth="800"
+      :stageWidth="300"
+      :stageHeight="650"
+      :particleCount="70"
+      :particleSize="6"
       :duration="3000"
     />
-    <p class="mb-6 pl-8 text-9xl">🎉</p>
+    <p class="motion-preset-shake motion-duration-2000 z-20 mb-6 pl-8 text-9xl">
+      🎉
+    </p>
     <p>
       The bright future has been set for you! Start learning with RunCloud Edu.
     </p>
