639b195e63a442376c8eb7bfe3e8d621ca5e1076
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-12-08 23:36:06 +0800

    feat(ui): update profile card ui

--- resources/js/components/profile/ProfileCard.vue
+++ resources/js/components/profile/ProfileCard.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import { Building, Building2, Cake, Mail, PartyPopper } from 'lucide-vue-next'
+import { BriefcaseBusiness, Cake, Mail } from 'lucide-vue-next'
 import { Button } from '../ui/button'
-import { Card, CardContent, CardFooter } from '../ui/card'
+import { Card, CardContent } from '../ui/card'
 import { Separator } from '../ui/separator'
+import { Link, usePage } from '@inertiajs/vue3'
+
+const authUser: AuthUser = usePage().props.auth.user
 
 const props = defineProps<{
   user: User
@@ -10,19 +13,6 @@ const props = defineProps<{
 </script>
 
 <template>
-  <!-- <div class="w-full rounded-2xl shadow-taper">
-    <div class="relative h-24 w-full rounded-t-2xl border bg-primary">
-      <div class="absolute -bottom-1/2 left-6 w-24">
-        <AspectRatio :ratio="1 / 1" class="rounded-2xl bg-muted">
-          <img :src="user.avatar" class="h-full w-full rounded-2xl border" />
-        </AspectRatio>
-      </div>
-    </div>
-
-    <div
-      class="h-72 w-full rounded-b-2xl border-b border-l border-r bg-card"
-    ></div>
-  </div> -->
   <Card class="mx-auto w-full max-w-md overflow-hidden">
     <div class="relative">
       <div
@@ -39,13 +29,22 @@ const props = defineProps<{
       </div>
     </div>
     <CardContent class="pb-4 pt-12">
-      <div class="mb-4 flex items-start justify-between">
+      <div class="mb-4 flex flex-col gap-4">
         <div>
           <h2 class="text-2xl font-bold">{{ props.user.full_name }}</h2>
           <p class="text-sm text-muted-foreground">
             @{{ props.user.username }}
           </p>
         </div>
+        <Link :href="route('settings')">
+          <Button
+            v-if="authUser.id === props.user.id"
+            variant="secondary"
+            class="w-full"
+          >
+            Edit Profile
+          </Button>
+        </Link>
       </div>
       <Separator class="my-2" />
       <div class="text-sm">
@@ -56,17 +55,23 @@ const props = defineProps<{
             {{ props.user.email }}
           </p>
           <p class="flex items-center gap-1">
-            <Building class="size-4" />
+            <BriefcaseBusiness class="size-4" />
             {{ props.user.occupation }}
           </p>
           <p class="flex items-center gap-1">
             <Cake class="size-4" />
-            {{ props.user.date_of_birth }}
+            {{
+              new Date(props.user.date_of_birth).toLocaleDateString('en-UK', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric',
+              })
+            }}
           </p>
         </div>
       </div>
       <Separator class="my-2" />
-      <div>
+      <div class="my-2">
         <h3 class="text-md font-bold">Badges</h3>
         <div class="mt-2 grid grid-cols-4 gap-2">
           <img
