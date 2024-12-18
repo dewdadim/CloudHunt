ea4f7d1ac8ea07f2307a7a11dc96e7ff22bdb74b
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-12-18 17:42:07 +0800

    chore: add more module for testing

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -13,6 +13,19 @@ class ModuleSeeder extends Seeder
     public function run(): void
     {
         \App\Models\Module::firstOrCreate(
+            ['id' => '9dc0d27c-3f79-441d-a65d-b2eec53e6e2d'],
+            [
+                'id' => '9dc0d27c-3f79-441d-a65d-b2eec53e6e2d',
+                'uri' => 'cloud-services-category',
+                'title' => 'Cloud Services Category',
+                'description' => '0',
+                'lesson_id' => '9dc0cdf3-abd6-4113-8361-3a72b5bcd327',
+                'category' => 'Learn',
+                'difficulty' => 'Easy'
+            ]
+        );
+
+        \App\Models\Module::firstOrCreate(
             ['id' => '9dc0ce06-58d4-496e-8953-73ad406fed9d'],
             [
                 'id' => '9dc0ce06-58d4-496e-8953-73ad406fed9d',
---
+++ resources/js/components/lessons/fundamentals-of-cloud-computing/cloud-services-category/index.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import MaxWidthWrapper from '@/components/MaxWidthWrapper.vue'
+import ModuleFinishButton from '@/components/ModuleFinishButton.vue'
+import { Button } from '@/components/ui/button'
+import { onMounted, ref } from 'vue'
+
+// Import task component here
+import TaskExample from './TaskExample.vue'
+
+// Add imported tasks here
+const tasks = [TaskExample]
+
+
+const props = defineProps<{
+  lesson: Lesson
+  module: Module
+}>()
+
+const emit = defineEmits<{
+  (event: 'update-progress', current: number, total: number): void
+}>()
+
+onMounted(() => {
+  emit('update-progress', 1, tasks.length)
+})
+
+const currentTaskIndex = ref(0)
+const visibleSections = ref(1)
+
+const handleTaskComplete = () => {
+  if (visibleSections.value < tasks.length) {
+    visibleSections.value++
+    currentTaskIndex.value++
+    emit('update-progress', currentTaskIndex.value + 1, tasks.length)
+
+    setTimeout(() => {
+      const sections = document.querySelectorAll('#task-container')
+      const lastSection = sections[sections.length - 1]
+      lastSection?.scrollIntoView({ behavior: 'smooth' })
+    }, 0)
+  }
+}
+</script>
+
+<template>
+  <MaxWidthWrapper class="md:max-w-screen-md">
+    <div class="space-y-10">
+      <section
+        id="task-container"
+        v-for="(task, index) in tasks.slice(0, visibleSections)"
+        :key="index"
+        class="flex min-h-[calc(100vh-80px)] flex-col justify-start py-20"
+      >
+        <component :is="task" :onComplete="handleTaskComplete" />
+        <div class="mt-6">
+          <Button
+            v-if="index === visibleSections - 1 && index < tasks.length - 1"
+            @click="handleTaskComplete"
+            variant="secondary"
+            size="lg"
+          >
+            Now I understand
+          </Button>
+          <ModuleFinishButton
+            v-if="index === visibleSections - 1 && index === tasks.length - 1"
+            :lesson="props.lesson"
+            :module="props.module"
+          />
+        </div>
+      </section>
+    </div>
+  </MaxWidthWrapper>
+</template>
