270642d14011775dae4d2de4ccbfdd4eb8bec136
Nadim Hairi <nadimhairi@hotmail.com>
2024-12-24 00:44:47 +0800

    chore: update test module

--- resources/js/components/lessons/test-lesson/test-module/TaskExample.vue
+++ resources/js/components/lessons/test-lesson/test-module/TaskExample.vue
@@ -7,8 +7,10 @@ defineProps<{
 </script>
 
 <template>
-  <div class="space-y-6">
-    <p>Try to list all files and directories using terminal.</p>
-    <XTerm />
+  <div class="space-y-10">
+    <p class="text-xl font-semibold">
+      Try to list all files and directories using terminal.
+    </p>
+    <XTerm dissable-help-command />
   </div>
 </template>
--- resources/js/components/lessons/test-lesson/test-module/TaskTwo.vue
+++ resources/js/components/lessons/test-lesson/test-module/TaskTwo.vue
@@ -2,8 +2,10 @@
 import XTerm from '@/components/XTerm.vue'
 </script>
 <template>
-  <div class="space-y-6">
-    <p>Now try to find "Hello.txt" and show the content in terminal.</p>
-    <XTerm />
+  <div class="space-y-10">
+    <p class="text-xl font-semibold">
+      Now try to find "Hello.txt" and show the content in terminal.
+    </p>
+    <XTerm dissable-help-command />
   </div>
 </template>
