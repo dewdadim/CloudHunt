0d07a3f58b2861b8d724ec2e9017fc0d314eece3
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2025-01-24 15:53:05 +0800

    chore: content management

--- resources/js/components/lessons/test-lesson/test-module/index.ts
+++ resources/js/components/lessons/test-lesson/test-module/index.ts
@@ -1,4 +1,17 @@
-import TaskExample from './TaskExample.vue'
-import TaskTwo from './TaskTwo.vue'
+import TaskLs from './TaskLs.vue'
+import TaskCatHello from './TaskCatHello.vue'
+import TaskPwd from './TaskPwd.vue'
+import TaskEcho from './TaskEcho.vue'
+import TaskTouch from './TaskTouch.vue'
+import TaskMakedir from './TaskMakedir.vue'
+import TaskChmod from './TaskChmod.vue'
 
-export const tasks = [TaskExample, TaskTwo]
+export const tasks = [
+  TaskLs,
+  TaskPwd,
+
+  TaskCatHello,
+  TaskEcho,
+  TaskTouch,
+  TaskMakedir,
+]
